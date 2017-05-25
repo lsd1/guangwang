a6562b45719a0a47fe83d35bf8956158b80daa19
stone <2416711130@qq.com>
2017-05-25 19:05:19 +0800

    12

--- controller/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-class database {
-    protected $pdo;
-    function __construct()
-    {
-        $this->pdo = new pdo("mysql:host=".HOSTNAME.";dbname=".DBNAME,USER,PWD);
-        $this->pdo->exec("set names utf8");
-    }
-}
--- index.php
+++
@@ -1,2 +0,0 @@
-<?php
-require_once './config/autoload.php';
