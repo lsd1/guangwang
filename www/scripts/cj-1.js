814f1628558470cbb34a9c0e699696e1d6b1b600
stone <2416711130@qq.com>
2017-08-30 21:06:49 +0800

    12

---
+++ www/scripts/cj-1.js
@@ -0,0 +1,62 @@
+var nametxt = $('.name'),
+ phonetxt = $('.phone'),
+ running = true,
+ num,
+ t,//定时器
+ counts = 0;
+
+//开始停止
+function start() {
+    if(running == false ){
+        return false;
+    }
+    running = false;
+    startNum();
+    $('#sound_play')[0].play();
+}
+//循环参加名单
+function startNum() {
+	num = Math.floor(Math.random() * (phone.length-1));
+    var target = phone[num];
+    if (target == undefined) {
+        startNum();
+    }
+    phonetxt.html(target);
+    t = setTimeout(startNum, 0);
+}
+//停止跳动,打印中奖者名单
+function show() {
+	running = true;
+    clearInterval(t);
+    t = 0;
+	phonetxt.html(phonenum);
+    $('.list').prepend("<p>" + lottery_type + ":" + phonenum + "</p>");
+	if(td  <= 0){
+		setTimeout(function(){
+            alert("开奖结束");
+        },1000);
+	}
+}
+//查询抽奖概率。
+function search(){
+    var maiguo_num = $('#maiguo_num').val();
+    if(maiguo_num == ''){alert('请输入查询号码!');return false;}
+    var allcounts = phone.length;
+    if(allcounts == 0){
+        alert('抽奖还未开始！不能查询！');return false;
+    }else{
+        for(i in phone){
+            if(phone[i] == maiguo_num){
+                counts++;
+            }
+        }
+        if(counts == 0){
+            alert('0次');return false;
+        }
+    }
+    alert(counts.toString()+'次');
+    counts = 0;
+}
+function shut() {
+}
+
---
+++ www/scripts/hichat.js
@@ -0,0 +1,104 @@
+/*
+ *抽奖
+ */
+var status = false,
+ that,
+ phone = [],
+ td,
+ phonenum,
+ list,
+ lottery_type = '';//抽奖类型。1：一等奖；2：二等奖；3：三等奖；4：四等奖。
+window.onload = function() {
+    var hichat = new HiChat();
+    hichat.init();
+};
+var HiChat = function() {
+    this.socket = null;
+};
+HiChat.prototype = {
+    init: function() {
+        that = this;
+        this.socket = io.connect();
+        //获取抽奖名单
+        this.socket.on('sendData', function(arg_phone,arg_td,arg_status,arg_lottery_type,arg_list) {
+			console.log('链接成功！');
+            lottery_type = arg_lottery_type;
+            phone = arg_phone;
+			td = arg_td;
+			status = arg_status;
+			lottery_type = arg_lottery_type;
+			list = arg_list;
+            console.log(arg_phone);
+            console.log(arg_td);
+            console.log(arg_status);
+            console.log(arg_lottery_type);            
+			console.log(arg_list);
+			if(td<1){
+			    $(" #btn_replay").show();
+            }else{
+                $(" #btn_replay").hide();
+            }
+            if(arg_status){//连上服务器之后如果抽奖状态为1，则说明抽奖进行中，立即启动抽奖。
+                start();
+            }
+			//刚联接上就更新获奖列表
+			if(arg_list){
+                //获取最新中奖号码显示出来。
+                last_phone = arg_list.split("</p>")[0].split(':')[1];
+            }else{
+                //如果还没有中奖结果则显示为000...
+                last_phone = '00000000000';
+            }
+            document.getElementById('last_phone').innerHTML = last_phone;
+            document.getElementById('luck_list').innerHTML = arg_list;
+        });
+        var patt = /admin/;
+        var res = patt.test(window.location.pathname);
+        if(res){
+            that.socket.emit('isAdmin','');
+        }
+        //单点登录
+        this.socket.on('close',function(){
+            shut();
+        });
+       
+        //捕获服务器抽奖事件。
+		this.socket.on('running', function(arg_type,arg_td,arg_phone,arg_phonenum,arg_lottery_type) {
+			console.log(arg_type);
+			console.log(arg_td);
+			console.log(arg_phone);
+			console.log(arg_phonenum);			
+			console.log(arg_lottery_type);
+			lottery_type = arg_lottery_type;
+            if(arg_type == 'start'){
+                start();
+            }else if(arg_type == 'stop'){
+				td = arg_td;
+				phone = arg_phone;
+                phonenum = arg_phonenum;
+                show();
+            } 
+        });
+		//清空抽奖列表，重新开始。
+		document.getElementById('btn_replay').addEventListener('click',function(){
+		    //清空抽奖列表，抽奖人数，抽奖状态。
+            that.socket.emit('reStart','');
+        });
+        //上传抽奖列表，重新开始。
+        /*document.getElementById('btn_check').addEventListener('click',function(){
+            var td = document.getElementById('count').value;
+            var text = document.getElementById('userlist').value;
+            if (text == "") return;
+            var temp_text = text.split("\n");
+            var target = new Array();
+            var rever_target = new Array();
+            for (var i = 0; i < temp_text.length; i++) {
+                if (temp_text[i] != "") {
+                    target.push(temp_text[i]);
+                }
+            }
+            lottery_type = document.getElementById('lottery_type').value;
+            that.socket.emit('getData',target,td,false,lottery_type,zd,'');
+        });*/
+	}
+};
